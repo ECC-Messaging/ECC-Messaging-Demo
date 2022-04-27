319cb960882504982d65293bb60a74841783d2b4
Joe Conwell <jmc529@vt.edu>
2022-04-26 20:46:35 -0400

    removed pantry packages and am using fetch

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,40 @@
 <script lang="ts">
+import { get } from "svelte/store";
+
 	import Post from "./components/Post.svelte";
 	import Spinner from "./components/Spinner.svelte";
-	import { Pantry } from 'pantry-cloud';
-	const pantry = new Pantry("3140d297-fd8e-4581-90f9-c879e38e26dd");
+	let posts = [];
 
-
-	async function getBasket() {
-		return await pantry.getBasket('messages');
+	async function getBasket() {				
+		const res = await 
+			fetch(`https://getpantry.cloud/apiv1/pantry/3140d297-fd8e-4581-90f9-c879e38e26dd/basket/messages`, 
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+		const json = await res.json();
+		if (res.ok) {
+			setTimeout(() => {
+				posts = json;			
+				
+				return true;
+			}, 0 * Math.random())
+		} else {
+			throw new Error("whoops");
+		}
 	}
-	const promise = getBasket;
+
+	let promise = getBasket();
 </script>
 
 {#await promise}
 	<Spinner/>
 {:then basket}
-<p>{basket}</p>
-	<!-- {#each basket as message}
-		<Post message={message.message} time={messages.timestamp}/>
+	<p>{basket}</p>
+	<!-- {#each posts as post}
+		<Post message={post.message} time={post.timestamp}/>
 	{/each} -->
 {:catch error}
 	<p style="color: red">{error}</p>
--- src/components/NewPost.svelte
+++ src/components/NewPost.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
     import { now } from "svelte/internal";
-    import { Pantry } from 'pantry-cloud';
 	const ecc = require('ecc-messaging-scheme-package');
-
     let message = '';
-	const pantry = new Pantry(process.env.PANTRY_ID);
+
+    async function updateBasket(message: string) {				
+		const res = await 
+        fetch(`https://getpantry.cloud/apiv1/pantry/3140d297-fd8e-4581-90f9-c879e38e26dd/basket/messages`, 
+			{
+				method: 'PUT',
+                body: JSON.stringify({
+                    message: message,
+                    timestamp: now()
+                }),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+		if (!res.ok) {
+			throw new Error();
+		}
+	}
 	
+
     async function handlePost() {
         ecc.generateSharedKey(process.env.ECC_PRIVATE_KEY);
         let encryptedMessage = ecc.encyrpt(message);
 
-        await pantry.putBasket('messages', 
-        { 
-            content: {
-                message: encryptedMessage, 
-                timestamp: now()
-            }
-        });
+        await updateBasket(encryptedMessage);
 	}
 </script>
 
