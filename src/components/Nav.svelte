19b6ad0641238408f40387c66fbfdf091e4e5634
Joe Conwell <jmc529@vt.edu>
2022-04-28 17:08:38 -0400

    nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import ECCM from 'ecc-messaging-scheme-package';
+    import GithubLogin from "svelte-github-login";
 	import { serverKey } from '../store.js';
 	import { user } from '../store.js';
 
@@ -10,10 +11,6 @@
         await generateSharedKeyWithUser();
     }
 
-    function handleSignup() {
-
-    }
-
     async function generateSharedKeyWithUser() {
         const key = await serverKey.get();
         const uuidECC = new ECCM(uuid);
@@ -22,6 +19,14 @@
 </script>
 
 <nav>
-  <a on:click={handleLogin}>Login</a>
-  <a on:click={handleSignup}>Signup</a>
+    <GithubLogin
+        clientId="Iv1.49a4894cc424a43a"
+        scope="user:email"
+        redirectUri="https://ecc-messaging-demo.surge.sh/"
+        on:success={params => console.log(params)}
+        on:error={error => console.log(error)}
+        let:onLogin
+    >
+        <button on:click={onLogin}>Github Login</button>
+    </GithubLogin>
 </nav>
