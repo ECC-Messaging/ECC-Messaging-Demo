0ab49f8520fee6ae828d41d6d4f5722c38dc0594
Joe Conwell <jmc529@vt.edu>
2022-04-28 16:40:22 -0400

    looking simple

---
+++ src/components/Nav.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import ECCM from 'ecc-messaging-scheme-package';
+	import { serverKey } from '../store.js';
+	import { user } from '../store.js';
+
+    let uuid: string;
+    let password: string;
+
+    async function handleLogin() {
+        await generateSharedKeyWithUser();
+    }
+
+    function handleSignup() {
+
+    }
+
+    async function generateSharedKeyWithUser() {
+        const key = await serverKey.get();
+        const uuidECC = new ECCM(uuid);
+        return uuidECC.generateSharedKey(key);
+    }
+</script>
+
+<nav>
+  <a on:click={handleLogin}>Login</a>
+  <a on:click={handleSignup}>Signup</a>
+</nav>
