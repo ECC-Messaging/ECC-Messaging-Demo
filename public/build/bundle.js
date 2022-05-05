6030b1dd1ecc88ab7db2e530fd00b6e4959ea892
Joe Conwell <jmc529@vt.edu>
2022-05-05 19:12:30 -0400

    updated package

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -1021,7 +1021,9 @@ var app = (function (crypto) {
             this.loadPublicKey();
         }
         generatePrivateKey() {
-            this.privateKey = 378;
+            let min = 1;
+            let max = 1000;
+            this.privateKey = Math.floor(Math.random() * (max - min + 1) + min);
             //ALTERNATE OPTION
             // localStorage.setItem("lastname", "Smith");
             // console.log(localStorage.getItem("lastname"));
